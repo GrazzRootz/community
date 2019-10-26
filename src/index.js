478664282e5cfd1f5d7b763103f0deee6bf13455
Sam Marshall <sjmarshy@gmail.com>
2019-10-26 15:38:06 +0100

    add auth

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-const express = require('express');
+const express  = require('express');
+const jwt      = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRSA   = require('jwks-rsa');
 
 const app = express();
 
-app.get('*', (_, res) => {
+const checkJWT = jwt({
+    secret: jwksRSA.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://dev-112l54tu.eu.auth0.com/.well-known/jwks.json'
+    }),
+    audience: 'grazzrootzcommunity',
+    issuer: 'https://dev-112l54tu.eu.auth0.com/',
+    algorithms: ['RS256']
+})
+
+
+const checkDiaryAuth = jwtAuthz(['read:diary']);
+const checkCalendarAuth = jwtAuthz(['read:calendar']);
+
+app.get('*', checkJWT, checkDiaryAuth, (_, res) => {
     return res.send('Hey!');
 });
 
